91764650592698bcb6ed9f0c9a0a5187f5d59413
IDGS-805-21000417 <119087603+TristanTorres@users.noreply.github.com>
2025-01-14 18:48:23 -0600

    Commit 14 de enero funciones

---
+++ tem4_while.py
@@ -0,0 +1,17 @@
+#Manejo de IF ELSE 
+y=0
+while y<=10:
+    print(y)
+    y=y+1    
+
+x= input('Ingresa un numero: ')
+z = 0
+while z<=10:
+    res = int(x) * int(z) 
+
+    print(x, " x ", z, " = ", res) 
+
+    res = 0
+
+    z=z+1
+    
